0c3b2bebde088e3f1f391121aafd411359e4ed76
Dylan Qiu <dylanksl@yahoo.com>
2022-02-21 17:02:51 +0800

    Part #3 Completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import pokemon from './pokemon.json';
 
 function App() {
   return (
@@ -18,10 +20,15 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Bulbasaur</td>
-            <td>Grass, Poison</td>
-          </tr>
+          {
+            pokemon.slice(0, 20).map(pokemon => (
+            <tr key={pokemon.id}>
+              <td>{pokemon.name.english}</td>
+              <td>{pokemon.type.join(", ")}</td>
+            </tr>
+            ))
+          }
+          
         </tbody>
       </table>
 
