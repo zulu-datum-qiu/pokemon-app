6d9a8e89d396d10b4f86e0f44e64ba34af9f647a
Dylan Qiu <dylanksl@yahoo.com>
2022-02-21 17:18:52 +0800

    Part #4 Completed
    Introduce basic Component concept
    Introduce the use of PropTypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './App.css';
 import pokemon from './pokemon.json';
+
+const PokemonRow = ({pokemon}) => {
+  return (
+    <tr>
+      <td>{pokemon.name.english}</td>
+      <td>{pokemon.type.join(", ")}</td>
+    </tr>
+  )
+}
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string
+    }),
+    type: PropTypes.arrayOf(PropTypes.string)
+  })
+}
 
 function App() {
   return (
@@ -22,10 +42,7 @@ function App() {
         <tbody>
           {
             pokemon.slice(0, 20).map(pokemon => (
-            <tr key={pokemon.id}>
-              <td>{pokemon.name.english}</td>
-              <td>{pokemon.type.join(", ")}</td>
-            </tr>
+              <PokemonRow pokemon={pokemon} key={pokemon.id} />
             ))
           }
           
